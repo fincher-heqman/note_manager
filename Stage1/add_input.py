df5de4d5aae594c210e4e50c0496b0cf08b61d05
Fincher <fincherheqman@gmail.com>
2025-01-20 09:06:40 +1000

    Решил 3 задание 1 модуля

---
+++ Stage1/add_input.py
@@ -0,0 +1,12 @@
+"""
+Grade 1. Этап 1: Задание 3
+"""
+from greetings import *
+
+# Инициализируем переменные
+username = input(f'Введите {username}: ')
+title = input(f'Введите {title}: ')
+content = input(f'Введите {content}: ')
+status = input(f'Введите {status}: ')
+created_date = input(f'Введите {created_date}: '.replace('дата', 'дату'))
+issue_date = input(f'Введите {issue_date}: '.replace('дата', 'дату'))
