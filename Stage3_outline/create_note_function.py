cf8344486579f6797cbb661c4796d45cb99f1a71
Fincher <fincherheqman@gmail.com>
2025-01-26 04:01:43 +1000

    Задание 1 Этапа 3 Grade 1 готово

--- Stage3_outline/create_note_function.py
+++ Stage3_outline/create_note_function.py
@@ -1,59 +1,100 @@
 """
 Grade 1. Этап 3: Задание 1
-Общее описание: Введение функций для упрощения кода и организации логики.
+Общее описание для Этапа 3: Введение функций для упрощения кода и организации логики.
+
+Настоящая create_note() функция: для создания новой заметки и возврата словаря.
 """
 import datetime
 
 
-# Добавляем дополнительно проверку к вводу пользователем на пустую строку
-def input_valid_str(text_for_user: str) -> str:
+### Блок функций ###
+## Вспомогательные функции для отсеивания некорректных значений ##
+# Для проверки на валидность строки после ввода
+def is_valid_str(user_input: str) -> bool:
+    # проверка строки на валидную в том, что строка не должна быть пустой
+    if not user_input:
+        return False
+    return True
+
+# Для запроса у пользователя валидной строки
+def input_valid_str(prompt: str) -> str:
+    while True:
+        user_input = input(prompt).strip()
+        if not is_valid_str(user_input):
+            print('Ошибка при введении значения, введите пожалуйста не пустое значение')
+            continue
+        user_input = user_input.capitalize()
+        return user_input
+
+# Для проверки на валидность значения изменения статуса
+def is_valid_status_choice(user_input: str) -> bool:
+    # проверка на соответствие допустимым значениям
+    available_values = ('выполнено', 'в процессе', 'отложено')
+    if user_input not in available_values:
+        return False
+    return True
+# Для запроса у пользователя значения изменения статуса
+def input_valid_status(prompt: str) -> str:
+    while True:
+        user_input = input(prompt).strip().lower()
+        if not is_valid_status_choice(user_input):
+            print('Ошибка при введении значения статуса, введите пожалуйста допустимое значение')
+            continue
+        return user_input
+
+# Для проверки на валидность значения даты
+def is_valid_date(user_input: str) -> bool:
+    # проверка на возможность преобразования указанной даты в объект
     try:
-        result = input(text_for_user)
-        result = result.strip()
-        if not result: raise ValueError
+        user_input_temp = user_input.split('.')
+        day = int(user_input_temp[0])
+        month = int(user_input_temp[1])
+        year = int(user_input_temp[2])
+        datetime.date(year, month, day)
+        return True
     except ValueError:
-        print('Ошибка ввода, не допускайте пожалуйста пустых значений')
-        result = input_valid_str(text_for_user)
-    result = result.capitalize()
-    return result
+        print(f'Ошибка в набранной дате {ValueError}. Пожалуйста, введите дату в соответствии с указанным форматом')
+        return False
+    except Exception as e:
+        print(f'Ошибка в набранной дате {e}. Пожалуйста, введите дату в соответствии с указанным форматом')
+        return False
 
-# Добавляем дополнительно проверку к вводу пользователем на валидность статуса
-def input_valid_status(text_for_user: str) -> str:
-    try:
-        result = input(text_for_user)
-        acceptable_values = [
-            'новая',
-            'в процессе',
-            'выполнено'
-        ]
-        result = result.strip().lower()
-        if result not in acceptable_values:
-            raise ValueError
-    except ValueError:
-        print('Ошибка ввода, введите пожалуйста одно из предложенных значений')
-        result = input_valid_status(text_for_user)
-    return result
+# Для запроса у пользователя значения даты
+def input_valid_date(prompt: str) -> str:
+    while True:
+        user_input = input(prompt)
+        if not is_valid_date(user_input):
+            print('Ошибка при введении даты, введите пожалуйста дату в соответствии с указанным форматом')
+            continue
+        user_input_temp = user_input.split('.')
+        day = int(user_input_temp[0])
+        month = int(user_input_temp[1])
+        year = int(user_input_temp[2])
+        user_input = datetime.date(year, month, day).strftime('%d.%m.%Y')
+        return user_input
 
-# Добавляем дополнительно проверку к вводу пользователем на валидность даты
-def input_valid_date(text_for_user: str) -> datetime.date:
-    try:
-        result = input(text_for_user)
-        result =  datetime.datetime.strptime(result, '%d-%m-%Y').date()
-    except ValueError:
-        print('Ошибка ввода, соблюдайте пожалуйста формат ввода даты')
-        result = input_valid_date(text_for_user)
-    return result
-
-# Запрашивает у пользователя информацию для создания заметки.
-
-# Функция создания заметки
+## Функция для непосредственного создания заметки ##
+# Запрашивает значения у пользователя через вспомогательные функции (указанные выше)
 def create_note() -> dict:
-    username = input_valid_str('Введите имя пользователя: ')
-    title = input_valid_str('Введите заголовок заметки: ')
-    content = input_valid_str('Введите описание заметки: ')
-    status = input_valid_status('Введите статус заметки (новая, в процессе, выполнено): ')
-    created_date = datetime.date.today().strftime('%d-%m-%Y')
-    issue_date = input_valid_date('Введите дедлайн (день-месяц-год): ').strftime('%d-%m-%Y')
+    # переменные для передачи в input()
+    prompt_username = 'Пожалуйста, введите имя пользователя: '
+    prompt_title = 'Пожалуйста, введите заголовок заметки: '
+    prompt_content = 'Пожалуйста, введите описание заметки: '
+    prompt_status = 'Пожалуйста, введите статус заметки из предложенных (выполнено, в процессе, отложено): '
+    prompt_issue_date = (
+        'Пожалуйста, введите дату истечения заметки (дедлайн) в формате "день.месяц.год", например "10.12.2024": '
+    )
+
+    # Запрашиваем все необходимые данные у пользователя и сохраняем в переменные
+    username = input_valid_str(prompt_username)
+    title = input_valid_str(prompt_title)
+    content = input_valid_str(prompt_content)
+    status = input_valid_status(prompt_status)
+    issue_date = input_valid_date(prompt_issue_date)
+    # Дата создания заполняется автоматически
+    created_date = datetime.date.today().strftime('%d.%m.%Y')
+
+    # Формируется словарь из полученных значений
     result = {
         'username': username,
         'title': title,
@@ -64,14 +105,21 @@ def create_note() -> dict:
     }
     return result
 
+## Функции для отображения пользователю информации о заметке и управления списком ##
+
 # Функция для структурированного отображения заметки
-def print_note(note: dict) -> None:
+def print_note(note: dict, num_note: int = None) -> None:
+    # Переменная num_note используется если нужно отобразить несколько заметок
     username = note.get('username')
     title = note.get('title')
     content = note.get('content')
     status = note.get('status')
     created_date = note.get('created_date')
     issue_date = note.get('issue_date')
+
+    print ('_' * 8)
+    if not num_note is None:
+        print(f'Заметка №{num_note}')
 
     print(f'Имя пользователя: {username}')
     print(f'Заголовок заметки: {title}')
@@ -80,10 +128,20 @@ def print_note(note: dict) -> None:
     print(f'Дата создания заметки: {created_date}')
     print(f'Дата истечения (дедлайна): {issue_date}')
 
-### Основная функция задания ###
+# Не нужная сейчас функция, сделал на будущее для отображения всех заметок
+def print_notes(notes_pool: list) -> None:
+    notes_pool = tuple(notes_pool)
+    count = 0
+    print('Список заметок: ')
+    for note in notes_pool:
+        print_note(note, count)
+    print('*' * 8)
+
+### Функция для демонстрации задания ###
 def main() -> None:
     current_note = create_note()
     print_note(current_note)
 
 if __name__ == '__main__':
+    # Через main() вызываем create_note()
     main()
